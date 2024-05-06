7bd666809a97cb7ae37f7ff82ea47de1ed419316
cristian jesus <christian.cuarite@tecsup.edu.pe>
2024-05-06 17:01:42 -0500

    Initial Commit

---
+++ ColeccionDeJuegos2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos2
+//
+//  Created by cristian jesus on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
